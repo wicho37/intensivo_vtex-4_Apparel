d2dd0313ca73ed3594a6693b73a0247a9f3956d6
wicho37 <willihernandezramirez@gmail.com>
2024-02-01 22:56:26 -0500

    se agrega el boseto para el input del email

--- src/components/Seccion_2/SeccionBelow.jsx
+++ src/components/Seccion_2/SeccionBelow.jsx
@@ -1,41 +1,46 @@
+import iconoError from "../../assets/icon-error.svg"
+import iconoFlecha from "../../assets/flecha.svg"
 
 
+function SeccionBelow() {
 
-
-function SeccionBelow () {
-    
     const [email, setEmail] = ("");
     const [mensError, setMensError] = ("");
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
     const handleClick = () => {
-        if ( email === "" || !regex.text(email)){
+        if (email === "" || !regex.text(email)) {
             setMensError("por favor ingresar email valido");
         }
     };
 
     return (
-         <div> 
-             <h1>we're</h1>
-             <h2>
+        <div>
+            <h1>we're</h1>
+            <h2>
                 coming <br />soon
-             </h2>
-             <p>Hello fellow shoppers! We're currently building oiur new fashion store. Add your email below to stay up-to date with announcements and our launchdeals.</p>
-             <div>
-                <input 
+            </h2>
+            <p>Hello fellow shoppers! We're currently building oiur new fashion store. Add your email below to stay up-to date with announcements and our launchdeals.</p>
+            <div>
+                <input
                     type="text"
                     value={email}
-                    onChange={(e) =>{
-                    setEmail(e.target.value);
-                    if (mensError !== "") {
-                        setMensError("");
-                    }
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                        if (mensError !== "") {
+                            setMensError("");
+                        }
                     }}
-                   placeholder="ingresa email" 
+                    placeholder="ingresa email"
                 />
                 <span className="">{mensError}</span>
+                {mensError !== "" && <img src={iconoError} className="" />}
             </div>
-         </div> 
+            <div onClick={handleClick}>
+                <img src={iconoFlecha} className="" alt="" />
+            </div>
+
+        </div>
     )
 }
 
