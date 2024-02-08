465a0f63fa78753203f66134816d1c00b3864bf6
wicho37 <willihernandezramirez@gmail.com>
2024-02-07 23:04:29 -0500

    se hace el css al input , menserror y el bottom de la validacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from './components/Header/Header'
 import SeccionAbove from "./components/Seccion1/SeccionAbove"
 import SeccionBelow from './components/Seccion_2/SeccionBelow'
 
-
 function App () {
   
 
@@ -12,6 +11,7 @@ function App () {
       <Header />
       <SeccionAbove />
       <SeccionBelow />
+      
 
       
     </>
--- src/components/Seccion_2/SeccionBelow.jsx
+++ src/components/Seccion_2/SeccionBelow.jsx
@@ -1,19 +1,21 @@
 import iconoError from "../../assets/icon-error.svg"
 import iconoFlecha from "../../assets/icon-arrow.svg"
 import "./SeccionBelow.css"
+import { useState } from "react";
 
 function SeccionBelow() {
 
-    const [email, setEmail] = ("");
-    const [mensError, setMensError] = ("");
+    const [email, setEmail] = useState("");
+    const [mensError, setMensError] = useState ("");
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
     const handleClick = () => {
-        if (email === "" || !regex.text(email)) {
-            setMensError("por favor ingresar email valido");
-        }
+        
+      if (email === "" || !regex.test(email)) {
+            setMensError("Please provide a valid email");
+        } 
     };
-
+  
     return (
         <div className="contentBelow">
             <h1>we're</h1>
@@ -21,25 +23,25 @@ function SeccionBelow() {
                 coming <br />soon
             </h2>
             <p>Hello fellow shoppers! We're currently building oiur new fashion store. Add your email below to stay up-to date with announcements and our launch deals.</p>
-            <div>
+            <div className="formContent">
                 <input
                     type="text"
-                    value={email}
+                    value={email} 
                     onChange={(e) => {
-                        setEmail(e.target.value);
+                        
+                       setEmail(e.target.value);
                         if (mensError !== "") {
                             setMensError("");
-                        }
+                        } 
                     }}
                     placeholder="Email Adress"
                 />
-                <span className="">{mensError}</span>
-                {mensError !== "" && <img src={iconoError} className="" />}
+                <span className="mensError">{mensError}</span>
+                {mensError !== "" && <img src={iconoError} className="iconoError" />}
+            <div onClick={handleClick} className="imgContent">
+                <img src={iconoFlecha} />
             </div>
-            <div onClick={handleClick}>
-                <img src={iconoFlecha} className="" alt="" />
-            </div>
-
+          </div>
         </div>
     )
 }
